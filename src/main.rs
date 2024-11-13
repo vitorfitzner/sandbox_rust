783e34a23beea2c96313c03aa2cd07e8ee8912c0
Vitor Fitzner <vitorti@outlook.com>
2024-11-13 11:18:05 -0300

    final guessing game

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,31 @@ use std::io;
 fn main() {
     println!("Jogo Advinha o número");
 
-    println!("Entre com um número");
+    loop {
+        println!("Entre com um número");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("falhou em ler a linha");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("falhou em ler a linha");
 
-    let guess : u32 = guess.trim().parse().expect("expected a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("Você advinhou: {}", guess);
 
-    let secret_number = 10;
+        let secret_number = 10;
 
-    match guess.cmp(&secret_number){
-        std::cmp::Ordering::Less => println!("Menor"),
-        std::cmp::Ordering::Greater => println!("Maior"),
-        std::cmp::Ordering::Equal => println!("Acertou mizeravi!")
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Menor"),
+            std::cmp::Ordering::Greater => println!("Maior"),
+            std::cmp::Ordering::Equal => {
+                println!("Acertou mizeravi!");
+                break;
+            },
+        }
     }
 }
